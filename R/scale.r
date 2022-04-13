97b32dab5a8e93f32ec10c5798ceb6cd1de5c474
user <lucky1515699@gmail.com>
2022-04-13 13:46:37 +0800

    change scale_sym into scale (generic function)

--- R/scale.r
+++ R/scale.r
@@ -1,25 +1,41 @@
-#' @name scale_sym
+#' @name scale
+#' @aliases scale
 #' @title scale for symbolic data table
 #' @description scale for symbolic data table
 #' @importFrom RSDA is.sym.interval
-#' @param data A ggESDA object. It can also be either RSDA object or
+#' @param x A ggESDA object. It can also be either RSDA object or
 #' classical data frame, which will be automatically convert to ggESDA
 #' data.
+#' @param center same as base::scale, either a logical value or numeric-alike vector of length equal to the number of columns of x, where ‘numeric-alike’ means that as.numeric(.) will be applied successfully if is.numeric(.) is not true.
+#' @param scale same as base::scale, either a logical value or a numeric-alike vector of length equal to the number of columns of x.
+#' @param ... Used by other R function.
 #' @return Return a scale ggESDA object.
-#' @usage scale.sym(data = NULL)
 #' @examples
 #'
 #' #For all interval-valued
-#' scale.sym(facedata)
+#' scale(facedata)
 #'
 #' #For both interval-valued and modal multi-valued
-#' scale.sym(mtcars.i)
+#' scale(mtcars.i)
 #'
+#' @keywords Symbolic scale
 #' @export
-scale_sym <- function(data = NULL){
+scale <- function(x, ...) {
+  UseMethod("scale")
+}
+
+#' @rdname scale
+#' @export
+scale.default <- function(x, center = TRUE, scale = TRUE, ...) {
+  base::scale.default(x, center, scale)
+}
+
+#' @rdname scale
+#' @export
+scale.symbolic_tbl <- function(x, ...){
 
   #test data illegal
-  ggSymData <- testData(data)
+  ggSymData <- testData(x)
   iData <- ggSymData$intervalData
 
 
@@ -53,4 +69,19 @@ scale_sym <- function(data = NULL){
   }
 
   return(myd)
+}
+
+#' @rdname scale
+#' @export
+scale.symbolic_interval <- function(x, ...){
+  s <- RSDA::sd(x)
+  m <- mean(x)
+  m1 <- (min(x) - m)/s
+  m2 <- (max(x) - m)/s
+  d <- data.frame(m1 = m1, m2 = m2)
+  d2 <- classic2sym(d, groupby = "customize",
+                    minData = d$m1,
+                    maxData = d$m2)
+
+  return(d2$intervalData$V1)
 }
